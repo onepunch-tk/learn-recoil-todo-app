2c9bde1682ac5102cc143c91fe58a098e2f81f1c
TaekyungHa <86tkstar@gmail.com>
2023-02-27 19:56:25 +0900

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {ThemeProvider} from "styled-components";
+import {darkTheme} from "./styles/theme";
+import {GlobalStyle} from "./styles/reset-css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <App />
+    <ThemeProvider theme={darkTheme}>
+        <GlobalStyle/>
+        <App />
+    </ThemeProvider>
 )
